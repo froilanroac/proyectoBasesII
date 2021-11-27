411d54f79338f696b8576fafcf6dfd35aad95d02
Froilan <froilanroac@gmail.com>
2021-11-27 14:29:20 -0400

    practica 4 lista

--- practicas/p4.sql
+++ practicas/p4.sql
@@ -1,19 +1,17 @@
+CREATE TABLE LOG_CREACIONES (
+    USUARIO VARCHAR(30),
+    FECHA_CREACION VARCHAR(30),
+    NOMBRE_TABLA VARCHAR(30)
+);
+/
 create or replace TRIGGER CREACION_TABLA
 after CREATE ON SCHEMA 
 BEGIN 
-dbms_output.put_line('-----USUARIO-----');
-dbms_output.put_line(USER);
-dbms_output.put_line('-----OBJ TYPE-----');
-dbms_output.put_line(ORA_DICT_OBJ_TYPE);
-dbms_output.put_line('-----OBJ NAME-----');
-dbms_output.put_line(ORA_DICT_OBJ_NAME);
-dbms_output.put_line('-----OBJ OWNER-----');
-dbms_output.put_line(ORA_DICT_OBJ_OWNER);
-dbms_output.put_line('-----SYSDATE-----');
-dbms_output.put_line(SYSDATE);
+    INSERT INTO LOG_CREACIONES VALUES (USER,SYSDATE,ORA_DICT_OBJ_NAME);
 END CREACION_TABLA;
 /
 
-DROP TABLE HOLA;
 SET SERVEROUTPUT ON;
 CREATE TABLE HOLA (ID NUMBER PRIMARY KEY);
+/
+SELECT * FROM LOG_CREACIONES;
